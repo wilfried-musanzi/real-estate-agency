9db155d672c41504b541183f60daa2f2ff126e9b
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-07-12 16:08:15 +0200

    Deploy v1

--- config/database.ts
+++ config/database.ts
@@ -20,7 +20,7 @@ const databaseConfig: DatabaseConfig = {
       connection: {
         host: Env.get('DB_HOST', DATABASE_URL.hostname),
         port: Env.get('DB_PORT', DATABASE_URL.port),
-        user: Env.get('DB_USERNAME', DATABASE_URL.username),
+        user: Env.get('DB_USER', DATABASE_URL.username),
         password: Env.get('DB_PASSWORD', DATABASE_URL.password),
         database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
       },
