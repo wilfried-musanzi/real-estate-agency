a9fdfee4ed82d070ded4954ee4c5e825a37f7a92
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-07-12 15:33:01 +0200

    Deploy v1

--- config/database.ts
+++ config/database.ts
@@ -7,6 +7,9 @@
 
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+import Url from 'url-parse'
+
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 const databaseConfig: DatabaseConfig = {
   /*
