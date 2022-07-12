51195f9de1b88efe00856b76664babcaa6636cf6
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-07-12 17:05:05 +0200

    Deploy v1

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: ENV_SILENT=true node build/server.js
-release: ENV_SILENT=true node build/ace migration:run --force
--- config/database.ts
+++ config/database.ts
@@ -1,10 +1,3 @@
-/**
- * Config source: https://git.io/JesV9
- *
- * Feel free to let us know via PR, if you find something broken in this config
- * file.
- */
-
 import Env from '@ioc:Adonis/Core/Env'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
 import Url from 'url-parse'
@@ -15,7 +8,7 @@ const databaseConfig: DatabaseConfig = {
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
-    heroku: {
+    prod: {
       client: 'pg',
       connection: {
         host: Env.get('DB_HOST', DATABASE_URL.hostname),
