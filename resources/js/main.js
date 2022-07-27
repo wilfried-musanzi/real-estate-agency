a3859ea38f7d805bb2272116a9bf448d6e7932c8
Wilfried Musanzi <musanziwilfried@gmail.com>
2022-07-27 20:20:46 +0200

    Mail

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,7 +1,10 @@
 import '../css/main.scss'
 import Toast from './toast'
 import Swup from 'swup'
+import SwupSlideTheme from '@swup/slide-theme'
 // import '@hotwired/turbo'
 
-new Swup()
+new Swup({
+  plugins: [new SwupSlideTheme()],
+})
 customElements.define('app-toast', Toast)
